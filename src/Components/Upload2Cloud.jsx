7d1b0bcf1e76d895ab04432a85bbf0e3fdd8f6e7
Kimber Ogden <mx.kimber@gmail.com>
2023-11-18 22:09:07 -0600

    Update Upload2Cloud component to include a file input form, update css for simple fun --> { Integrating Cloudinary without backend }

--- src/Components/Upload2Cloud.jsx
+++ src/Components/Upload2Cloud.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import "../App.css"
 
 export function Upload2Cloud() {
+
+  const uploadImage = (files) => {
+    console.log(files[0]);
+  }
+
   return (
     <div>
-      <h2>Upload to Cloudinary</h2>
-      (without rails backend)
+      <div className="heading">
+        <h2>Upload to Cloudinary</h2>
+        (without rails backend)
+      </div>
+      <div className="input">
+        <input 
+          type="file"
+          onChange={(event) => {
+            uploadImage(event.target.files);
+          }}
+        />
+      </div>
     </div>
   )
 }
